b4a910663b4831945cde3acbde9b67816d3a1244
YOUSEF YAGOUB YOUSEF ALSULIMAN <443105666@student.ksu.edu.sa>
2025-08-12 13:02:24 +0300

    Update to-do.js

--- to-do.js
+++ to-do.js
@@ -3,6 +3,7 @@ const listContainer = document.getElementById("list-container");
 
 // Add a new task
 const addTask = () => {
+  addButton.innerHTML = "Add";
   const inputValue = inputBox.value;
 
   if (inputValue === "") {
@@ -22,6 +23,19 @@ const addTask = () => {
   saveData();
 };
 
+const completeListElement = (content) => {
+  const li = document.createElement("li");
+  li.innerHTML = content;
+  const span = document.createElement("span");
+  span.innerHTML = "\u00d7";
+  li.appendChild(span);
+  const button = document.createElement("button");
+  button.innerHTML = "Edit";
+  li.appendChild(button);
+
+  return li;
+};
+
 // Handle click events on the task list
 listContainer.addEventListener("click", (e) => {
   if (e.target.tagName === "LI") {
@@ -34,12 +48,27 @@ listContainer.addEventListener("click", (e) => {
     const inputValue = e.target.parentElement.childNodes[0].nodeValue.trim();
     e.target.parentElement.remove();
     inputBox.value = inputValue;
+    const addButton = document.getElementById("addButton");
+    addButton.innerHTML = "Edit";
   }
 });
 
 // Save tasks to localStorage
 const saveData = () => {
-  localStorage.setItem("data", listContainer.innerHTML);
+  var content = [];
+
+  for (let i = 0; i < listContainer.children.length; i++) {
+    content[i] = listContainer.children[i].childNodes[0].textContent;
+  }
+  // content -> text inside li
+
+  const ul = document.createElement("ul");
+
+  // console.log(completeListElement(content[0]));
+  for (let i = 0; i < content.length; i++) {
+    ul.appendChild(completeListElement(content[i]));
+  }
+  localStorage.setItem("data", ul.innerHTML);
 };
 
 // Load saved tasks from localStorage
